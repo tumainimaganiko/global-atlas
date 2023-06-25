29fdc296c973c5d0bba400971faece443423a6e6
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-25 13:24:41 +0300

    Create Intial Data

--- src/redux/Home/homeSlice.js
+++ src/redux/Home/homeSlice.js
@@ -1,11 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    home:[]
-}
+  home: [
+    {
+      name: "Tanzania",
+      views: 782,
+      logo: "https://www.globalization-partners.com/wp-content/uploads/2015/03/globalpedia-hero-tanzania-1.jpg",
+    },
+    {
+      name: "Tanzania",
+      views: 782,
+      logo: "https://www.globalization-partners.com/wp-content/uploads/2015/03/globalpedia-hero-tanzania-1.jpg",
+    },
+    {
+      name: "Tanzania",
+      views: 782,
+      logo: "https://www.globalization-partners.com/wp-content/uploads/2015/03/globalpedia-hero-tanzania-1.jpg",
+    },
+    {
+      name: "Tanzania",
+      views: 782,
+      logo: "https://www.globalization-partners.com/wp-content/uploads/2015/03/globalpedia-hero-tanzania-1.jpg",
+    },
+    {
+      name: "Tanzania",
+      views: 782,
+      logo: "https://www.globalization-partners.com/wp-content/uploads/2015/03/globalpedia-hero-tanzania-1.jpg",
+    },
+    {
+      name: "Tanzania",
+      views: 782,
+      logo: "https://www.globalization-partners.com/wp-content/uploads/2015/03/globalpedia-hero-tanzania-1.jpg",
+    },
+    {
+      name: "Tanzania",
+      views: 782,
+      logo: "https://www.globalization-partners.com/wp-content/uploads/2015/03/globalpedia-hero-tanzania-1.jpg",
+    },
+    {
+      name: "Tanzania",
+      views: 782,
+      logo: "https://www.globalization-partners.com/wp-content/uploads/2015/03/globalpedia-hero-tanzania-1.jpg",
+    },
+  ],
+};
 const homeSlice = createSlice({
-    name: home,
-    initialState,
-    reducers: {},
-    extraReducers: {}
-})
+  name: home,
+  initialState,
+  reducers: {},
+  extraReducers: {},
+});
+
+export default homeSlice.reducer;
