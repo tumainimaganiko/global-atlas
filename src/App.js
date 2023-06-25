0a1a23d4d0aab72ebf19697a164e0c0de17785e5
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-25 19:24:25 +0300

    Update  Routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import { Outlet,Route,Routes } from 'react-router-dom';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import DetailsPage from './components/DetailsPage';
 import HomePage from './components/HomePage';
 import './App.css';
 
-const Layout = () => {
+const Layout = () => (
   <>
-    <Outlet/>
+    <Outlet />
   </>
-}
+);
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />}/>
-        <Route path="details" element={<DetailsPage />}/>
+        <Route index element={<HomePage />} />
+        <Route path="details" element={<DetailsPage />} />
       </Route>
     </Routes>
   );
