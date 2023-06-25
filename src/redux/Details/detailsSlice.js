70ff8243bd2ffc4e792e0c88609cf28d44749ffc
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-25 13:15:08 +0300

    Create Details Slice

---
+++ src/redux/Details/detailsSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    details: []
+}
+const detailsSlice = createSlice({
+    name:details,
+    initialState,
+    reducers:{},
+    extraReducers:{}
+})
