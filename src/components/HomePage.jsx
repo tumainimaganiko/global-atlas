159587e4101330bbeec45ee2b38ef51c52ac5170
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-25 19:22:42 +0300

    Dispatch Fetching Data

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styles from '../styles/HomePage.module.css';
+import { fetchCountries } from '../redux/Home/homeSlice';
 
 const HomePage = () => {
+  const { home } = useSelector((store) => store.home);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCountries());
+  }, [dispatch]);
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className={styles.container}>
+      {home.map((item) => (
+        <div>
+          {console.log(item.capital)}
+          <h2>Hnjmkm</h2>
+          name:
+          {' '}
+          {item.name.official}
+          Capital City:
+          {' '}
+          {item.capital[0]}
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
