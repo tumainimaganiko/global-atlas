1633b7d9045e6cd005635efb6401b0a5cf30a71c
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-25 19:21:58 +0300

    Import Home Reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-import HomeReducer from './Home/homeSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import HomeReducer from './Home/homeSlice';
 
 const store = configureStore({
-    reducer:{
-        home: HomeReducer
-    }
-})
+  reducer: {
+    home: HomeReducer,
+  },
+});
 
 export default store;
