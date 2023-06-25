a64d0fe8c688dba0c7f91f1b5ab74f6bd1258145
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-25 19:25:05 +0300

    Update Store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { Provider } from "react-redux";
+import App from "./App";
+import store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-      <App />
-      </Provider>      
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
