9e5f9902a8dc8d61f534d37e75c002abd79225e3
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-25 13:17:16 +0300

    Create Home Slice

---
+++ src/redux/Home/homeSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    home:[]
+}
+const homeSlice = createSlice({
+    name: home,
+    initialState,
+    reducers: {},
+    extraReducers: {}
+})
