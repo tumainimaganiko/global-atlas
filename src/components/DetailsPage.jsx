071d3be9fd7343f9e7b499494cc8f30ee9f226cc
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-26 16:14:16 +0300

    Add Back icon

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -1,34 +1,39 @@
 import React from 'react';
 import { useLocation } from 'react-router';
+import { NavLink } from 'react-router-dom';
+import { FaAngleLeft } from 'react-icons/fa';
 
 const DetailsPage = () => {
   const location = useLocation();
   const { country } = location.state;
   return (
     <div>
-      Capital City:
-      {' '}
-      {country.capital[0]}
-      {' '}
-      <br />
-      Continent:
-      {' '}
-      {country.continents}
-      {' '}
-      <br />
-      Flag:
-      {' '}
-      {country.flag}
-      {' '}
-      <br />
-      Languages:
-      {' '}
-      {country.languages.eng}
-      {' '}
-      <br />
-      Population:
-      {' '}
-      {country.population}
+      <NavLink to="/"><FaAngleLeft /></NavLink>
+      <div>
+        Capital City:
+        {' '}
+        {country.capital[0]}
+        {' '}
+        <br />
+        Continent:
+        {' '}
+        {country.continents}
+        {' '}
+        <br />
+        Flag:
+        {' '}
+        {country.flag}
+        {' '}
+        <br />
+        Languages:
+        {' '}
+        {country.languages.eng}
+        {' '}
+        <br />
+        Population:
+        {' '}
+        {country.population}
+      </div>
     </div>
   );
 };
