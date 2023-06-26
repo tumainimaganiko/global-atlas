0c759fcd112b265ebde4245d2c66122099e6e09c
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-26 10:44:41 +0300

    Pass Prop to DetailsPage component

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useLocation } from 'react-router';
 
-const DetailsPage = () => (
-  <div>DetailsPage</div>
-);
+const DetailsPage = () => {
+  const location = useLocation();
+  const { country } = location.state;
+  console.log(country);
+  console.log(country.capital);
+  return (
+    <div>
+      Capital City: {country.capital[0]} <br />
+      Continent: {country.continents} <br />
+      Flag: {country.flag} <br />
+      Languages: {country.languages.eng} <br />
+      Population: {country.population}
+    </div>
+  );
+};
 
 export default DetailsPage;
