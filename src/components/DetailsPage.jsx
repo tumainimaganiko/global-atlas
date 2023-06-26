7493c310818740fe366b891289a0c0857cb70787
tumainimaganiko <tumainimaganiko@gmail.com>
2023-06-26 19:44:03 +0300

    Add Conditional rendering

--- src/components/DetailsPage.jsx
+++ src/components/DetailsPage.jsx
@@ -1,38 +1,75 @@
 import React from 'react';
 import { useLocation } from 'react-router';
 import { NavLink } from 'react-router-dom';
-import { FaAngleLeft } from 'react-icons/fa';
+import { FaRegArrowAltCircleRight, FaAngleLeft } from 'react-icons/fa';
+import styles from '../styles/DetailsPage.module.css';
 
 const DetailsPage = () => {
   const location = useLocation();
   const { country } = location.state;
   return (
-    <div>
+    <div className={styles.container}>
       <NavLink to="/"><FaAngleLeft /></NavLink>
       <div>
-        Capital City:
-        {' '}
-        {country.capital[0]}
-        {' '}
-        <br />
-        Continent:
-        {' '}
-        {country.continents}
-        {' '}
-        <br />
-        Flag:
-        {' '}
-        {country.flag}
-        {' '}
-        <br />
-        Languages:
-        {' '}
-        {country.languages.eng}
-        {' '}
-        <br />
-        Population:
-        {' '}
-        {country.population}
+        <h2>{country.name.official}</h2>
+        <p className={styles.flag}>{country.flag}</p>
+        <div className={styles.details}>
+          {country.capital[0] && (
+            <p>
+              Capital City
+              <span>
+                {country.capital[0]}
+                {' '}
+                <span><FaRegArrowAltCircleRight /></span>
+              </span>
+            </p>
+          )}
+          { country.continents && (
+            <p>
+              Continent
+              <span>
+                {country.continents}
+                <span><FaRegArrowAltCircleRight /></span>
+              </span>
+            </p>
+          )}
+          {country.timezones && (
+            <p>
+              Timezone
+              <span>
+                {country.timezones[0]}
+                <span><FaRegArrowAltCircleRight /></span>
+              </span>
+            </p>
+          )}
+          {country.population !== 0 && (
+            <p>
+              Population
+              <span>
+                {country.population}
+                <span><FaRegArrowAltCircleRight /></span>
+              </span>
+            </p>
+          )}
+          {country.area && (
+            <p>
+              Area in square meters
+              <span>
+                {country.area}
+                <span><FaRegArrowAltCircleRight /></span>
+              </span>
+            </p>
+          )}
+          {country.subreagion && (
+            <p>
+              Sub region
+              <span>
+                {country.subregion}
+                <span><FaRegArrowAltCircleRight /></span>
+              </span>
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
